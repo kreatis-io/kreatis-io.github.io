10f8d378197cd5c4f3def7651b2f9193a3e1d350
Vithushan <63534975+ACuteWoof@users.noreply.github.com>
2023-10-07 12:00:40 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import Contact from "./sections/contact";
+// import Contact from "./sections/contact";
 import Features from "./sections/features";
 import Hero from "./sections/hero";
 import Pricing from "./sections/pricing";
@@ -13,7 +13,6 @@ export default function Home() {
       <Why />
       <Features />
       <Pricing />
-      <Contact />
       <footer className='p-4 md:flex md:items-center md:justify-between md:p-6 '>
         <span className='text-sm text-gray-500 sm:text-center dark:text-gray-400'>
           © 2023 ACuteWoof. All Rights Reserved.
